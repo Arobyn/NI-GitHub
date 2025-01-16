bc737a265c7d67d0561aaff78454991be2bb704a
Arobyn <piotr.stemplewski@onet.pl>
2025-01-16 16:40:13 +0100

    test

---
+++ Lab 12 dodawanie/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab_12_dodawanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double a, b;
+            Console.WriteLine("Podaj pierwszą liczbę ");
+            a = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę ");
+            b = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Suma podanych liczb to: "+(a+b));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NI-GitHub
 Lab nr 12
+Napiszę coś
+A nawet 2 cosie
